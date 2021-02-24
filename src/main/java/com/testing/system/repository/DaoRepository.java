ef73060e7901f68ea302ce09ce9ee956ab0c983c
S-Maks <cool.dan47@mail.ru>
2021-02-24 21:02:10 +0300

    Update DaoRepository, add findAll function

--- src/main/java/com/testing/system/repository/DaoRepository.java
+++ src/main/java/com/testing/system/repository/DaoRepository.java
@@ -1,8 +1,15 @@
 package com.testing.system.repository;
 
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DaoRepository<T> {
+    SessionFactory getBeanToBeAutowired();
 
+    default List<T> findAll(Class T) {
+        return getBeanToBeAutowired().getCurrentSession().createQuery("from " + T.getSimpleName()).list();
+    }
 }
