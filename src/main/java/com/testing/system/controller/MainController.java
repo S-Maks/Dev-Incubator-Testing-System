caec5f877d951b9796147b781c6bbbeec96615f4
zhennik26 <your_email@whatever.com>
2021-02-24 12:19:16 +0300

    Added Thymeleaf

--- src/main/java/com/testing/system/controller/MainController.java
+++ src/main/java/com/testing/system/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.testing.system.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/")
 public class MainController {
 
-    @GetMapping(value = "/")
-    public String homePage() {
+    @GetMapping
+    public String homePage(Model model) {
+        String name="MAX";
+        model.addAttribute("name",name);
         return "index";
     }
 }
