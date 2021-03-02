6d9c38d6723b23b1cae0e6a9f53eceed94e361fc
S-Maks <cool.dan47@mail.ru>
2021-03-02 14:31:25 +0300

    Add logout and redirect

--- src/main/java/com/testing/system/controller/MainController.java
+++ src/main/java/com/testing/system/controller/MainController.java
@@ -4,32 +4,56 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class MainController {
 
     @GetMapping(value = "/login")
-    public String loginPage(Model model) {
+    public String loginPage(Authentication authentication) {
+        if(authentication!=null){
+            return "redirect:/" +  redirectToHomePageRole();
+        }
         return "login";
     }
 
     @GetMapping(value = "/")
-    public String homePage(Authentication authentication) {
+    public String homePage() {
         return "index";
     }
 
-    private Object getPrincipal() {
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails) principal).getUsername();
-        } else {
-            userName = principal.toString();
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return userName;
+        return "redirect:/login?logout";
+    }
+
+    private String redirectToHomePageRole(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        switch (((UserDetails) principal).getAuthorities().toString()) {
+            case "[ROLE_ADMIN]" -> {
+                return "admin";
+            }
+            case "[ROLE_TUTOR]" -> {
+                return "tutor";
+            }
+            case "[ROLE_USER]" -> {
+                return "user";
+            }
+            default -> {
+                return "accessDenied";
+            }
+        }
     }
 }
